6b1f29b93088e8a1fa3ebb69bb5bdffa4fd367d7
lyb <lyb@chehaha.net>
2019-11-27 18:05:14 +0800

    提交layout

---
+++ src/components/layout/le-aside.vue
@@ -0,0 +1,27 @@
+<template> 
+  <aside class="le-aside" :style="{width: width || '100%', height:height}"><slot></slot></aside>  
+</template>
+
+<script>
+export default {
+  name: 'leAside',
+  props: {
+    width: {
+      type: String,
+      default: ''
+    },
+    height: {
+      type: String,
+      default: 'auto'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .le-aside {
+    flex:1 1 auto;
+    align-self: stretch;
+  }
+</style>
+
