6e45eab3007621f2f9cc9004fe1c2a857377fc4f
Brandon <brandon@mogul-tech.com>
2019-11-28 22:54:54 +0800

    example container init

--- example/App.vue
+++ example/App.vue
@@ -1,44 +1,78 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <router-view></router-view>
-    <vc-button></vc-button>
-    <hr>
-    <!--<vc-container width="500px" height="400px">-->
-      <!--<vc-header style="border: 1px solid yellow">-->
-        <!--<p>vc-header</p>-->
-      <!--</vc-header>-->
-      <!--<vc-main>-->
-        <!--<vc-aside style="border: 1px solid red">-->
-          <!--<p>vc-aside</p>-->
-        <!--</vc-aside>-->
-        <!--<vc-main style="border: 1px solid blue">-->
-          <!--<p>vc-main</p>-->
-        <!--</vc-main>-->
-      <!--</vc-main>-->
-      <!--<vc-footer style="border: 1px solid green">-->
-        <!--<p>vc-footer</p>-->
-      <!--</vc-footer>-->
-    <!--</vc-container>-->
-    <hr>
-    <!-- <button></button> -->
-    <!--<bb />-->
-    <layout></layout>
-  </div>
+  <vc-container class="app">
+    <vc-header class="app-header">
+      <img class="app-header-logo" alt="Vue logo" src="./assets/logo.png">
+      <h1 class="app-header-title">VC - Vue Components</h1>
+    </vc-header>
+    <vc-container class="app-container">
+      <vc-aside class="app-aside">
+        <ul>
+          <li v-for="(route, index) in routes" :key="index">
+            <router-link :to="route.path">{{route.meta.name}}</router-link>
+          </li>
+        </ul>
+      </vc-aside>
+      <vc-main class="app-main">
+        <router-view></router-view>
+      </vc-main>
+    </vc-container>
+  </vc-container>
 </template>
 
 <script>
-// import bb from './docs/bb.md'
-import layout from './docs/layout.md'
+import { routes } from './router'
 export default {
   name: 'app',
   components: {
-    // bb,
-    layout
+  },
+  data () {
+    return {
+      routes
+    }
   }
 }
 </script>
 
 <style lang="scss">
-  @import './assets/md.scss';
+  html,body{
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    font-family: Helvetica Neue,Helvetica,PingFang SC,Hiragino Sans GB,Microsoft YaHei,SimSun,sans-serif;
+    font-weight: 400;
+    -webkit-font-smoothing: antialiased;
+    -webkit-tap-highlight-color: transparent;
+  }
+</style>
+
+<style scoped="" lang="scss">
+  .app {
+    height: 100%;
+    &-header {
+      height: 60px;
+      z-index: 1500;
+      background-color: #FFF;
+      display: flex;
+      align-items: center;
+      box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+      &-logo {
+        margin-left: 20px;
+        width: 20px;
+      }
+      &-title {
+        margin: 0 20px;
+        font-size: 20px;
+      }
+    }
+    &-container {
+      height: calc(100% - 60px);
+    }
+    &-aside {
+      overflow-x: hidden;
+      width: 150px;
+    }
+    &-main{
+      overflow-x: hidden;
+    }
+  }
 </style>
--- example/main.js
+++ example/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VC from '../src'
+import './assets/md.scss'
 import '../src/styles/index.scss'
+import './demo-styles/index.scss'
 
 Vue.use(VC)
 
