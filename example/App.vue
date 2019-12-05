7d75e0b877210131b221d27411c654fa70af8eee
zepang <ummlq4@gmail.com>
2019-12-05 15:30:39 +0800

    完成主页的修改

--- example/App.vue
+++ example/App.vue
@@ -1,8 +1,15 @@
 <template>
   <vc-container class="app">
     <vc-header class="app-header">
-      <img class="app-header-logo" alt="Vue logo" src="./assets/logo.png">
-      <h1 class="app-header-title heading-middle">Vue Components</h1>
+      <div class="app-header__inner">
+        <img class="app-header-logo" alt="Vue logo" src="./assets/logo.png">
+        <h1 class="app-header-title heading-middle">Vue Components</h1>
+        <ul class="app-header-nav">
+          <li><router-link class="heading-small" to="/">Home</router-link></li>
+          <li><router-link class="heading-small" to="/">Components</router-link></li>
+          <li><a class="heading-small" href="/">Github</a></li>
+        </ul>
+      </div>
     </vc-header>
     <vc-container class="app-container">
       <router-view></router-view>
@@ -45,10 +52,63 @@ export default {
       background-color: #FFF;
       display: flex;
       align-items: center;
+      justify-content: center;
       transition: all .25s ease;
 
+      &__inner {
+        width: 100%;
+        max-width: 1400px;
+        height: 60px;
+        display: flex;
+        align-items: center;
+      }
+
+      &-nav {
+        margin-left: auto;
+
+        li {
+          display: inline-block;
+          padding: 0 20px;
+
+          position: relative;
+
+          list-style: none;
+
+          &:not(:last-child) {
+            margin-right: 20px;
+          }
+
+          &:hover {
+            a {
+              color: #5575c8;
+            }
+            a:after {
+              transform: scaleX(1);
+            }
+          }
+
+          a {
+            position: relative;
+            color: lighten(#5575c8, 10%);
+
+            &:after {
+              content: '';
+              display: inline-block;
+              width: 100%;
+              height: 3px;
+              position: absolute;
+              top: 130%;
+              left: 0;
+              background-color: #5575c8;
+              transform-origin: 50%;
+              transform: scaleX(0);
+              transition: all .2s .1s ease-in-out;
+            }
+          }
+        }
+      }
+
       &-logo {
-        margin-left: 40px;
         width: 60px;
       }
       &-title {
--- example/pages/home.vue
+++ example/pages/home.vue
@@ -1,18 +1,68 @@
 <template>
-  <div class="home-page clear">
-    <div class="desc">
-      <h3 class="heading-super">中后台快速开发的组件库</h3>
-      <p class="paragraph">VC, Vue Components 是一套基于 Vue 开发的组件库，集成大量常用的UI组件，帮助开发者快速完成PC端的中后台产品开发</p>
-      <vc-button size="small">快速开始</vc-button>
-      <vc-button type="info">快速开始</vc-button>
-      <vc-button type="success">快速开始</vc-button>
-      <vc-button type="warning" size="middle">快速开始</vc-button>
-      <vc-button type="error" size="large">
-        快速开始
-        <vc-icon name=arrow-right></vc-icon>
-      </vc-button>
+  <div class="home-page">
+    <div class="home-page__body">
+      <div class="banner clear">
+        <div class="desc">
+          <h3 class="heading-super">中后台快速开发的组件库</h3>
+          <p class="paragraph">Vue Components 是一套基于 Vue 开发的组件库，集成大量常用的UI组件，帮助开发者快速完成PC端的中后台产品开发</p>
+          <vc-button type="info" size="large">
+            快速开始
+            <vc-icon name=arrow-right></vc-icon>
+          </vc-button>
+        </div>
+        <img class="fr" src="../assets/svg/home-banner1.svg" alt="">
+      </div>
+      <div class="banner clear">
+        <ul class="feature-list fr">
+          <li class="feature-item">
+            <vc-icon name="radio-checked"></vc-icon>
+            <div class="content">
+              <p class="heading-middle">Free</p>
+              <p class="heading-middle">免费开源</p>
+            </div>
+          </li>
+          <li class="feature-item">
+            <vc-icon name="radio-checked"></vc-icon>
+            <div class="content">
+              <p class="heading-middle">Easy</p>
+              <p class="heading-middle">简单易用</p>
+            </div>
+          </li>
+          <li class="feature-item">
+            <vc-icon name="radio-checked"></vc-icon>
+            <div class="content">
+              <p class="heading-middle">Standard</p>
+              <p class="heading-middle">标准规范</p>
+            </div>
+          </li>
+          <li class="feature-item">
+            <vc-icon name="radio-checked"></vc-icon>
+            <div class="content">
+              <p class="heading-middle">Scalability</p>
+              <p class="heading-middle">拓展性强</p>
+            </div>
+          </li>
+          <li class="feature-item">
+            <vc-icon name="radio-checked"></vc-icon>
+            <div class="content">
+              <p class="heading-middle">......</p>
+            </div>
+          </li>
+        </ul>
+        <img class="fl" src="../assets/svg/home-banner2.svg" alt="">
+      </div>
+      <div class="banner clear">
+        <div class="contributor">
+          <h2 class="desc heading-large">
+            各位大佬我实在编不下去了, 请自行脑补吧 ...
+          </h2>
+        </div>
+        <img class="fr" src="../assets/svg/home-banner3.svg" alt="">
+      </div>
     </div>
-    <img src="../assets/svg/home-bg.svg" alt="">
+    <div class="home-page__footer">
+      &copy;2019 - VC (Vue Components) / interest-groups
+    </div>
   </div>
 </template>
 <script>
@@ -23,24 +73,100 @@ export default {
 
 <style lang="scss" scoped>
 .home-page {
-  width: 90%;
-  max-width: 1360px;
-  margin: 0 auto;
-  padding: 30px 0;
+  width: 100%;
 
-  position: relative;
-
-  .desc {
-    width: 36%;
-
-    position: absolute;
-    top: 50%;
-    transform: translateY(-50%);
+  .home-page__body {
+    width: 90%;
+    max-width: 1360px;
+    margin: 0 auto;
+    padding: 100px 0;
   }
 
-  img {
-    float: right;
-    width: 60%;
+  .banner {
+    position: relative;
+
+    &:not(:first-child) {
+      margin-top: 250px;
+    }
+
+    &:nth-child(1) {
+      img {
+        width: 60%;
+      }
+    }
+
+    &:nth-child(2) {
+      img {
+        width: 50%;
+      }
+    }
+
+    &:nth-child(3) {
+      img {
+        width: 60%;
+      }
+    }
+
+    .desc {
+      width: 36%;
+
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
+
+      p {
+        margin-top: 30px;
+        margin-bottom: 50px;
+      }
+    }
+  }
+
+  .feature-list {
+    width: 40%;
+    height: 100%;
+    margin: 0;
+
+    position: absolute;
+    right: 0;
+    top: 0;
+
+    font-size: 0;
+
+    .feature-item {
+    }
+  }
+
+  .feature-item {
+
+    & > * {
+      display: inline-block;
+      vertical-align: middle;
+      margin-left: 20%;
+      font-size: 30px;
+    }
+
+    &:not(:last-child) {
+      margin-bottom: 10%;
+    }
+
+    .content {
+      p {
+        margin-top: 10px;
+      }
+    }
+  }
+
+  .home-page__footer {
+    width: 100%;
+    height: 60px;
+    margin-top: 100px;
+
+    background-color: darken(#5575c8, 10%);
+
+    color: #ffffff;
+    font-size: 13px;
+    text-align: center;
+    line-height: 60px;
   }
 }
 </style>
