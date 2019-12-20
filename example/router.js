a847e7be6637eb8b3f6176a2596d2912581ec0e6
lyb <lyb@chehaha.net>
2019-12-20 18:03:59 +0800

    修改左边菜单栏分类

--- example/router.js
+++ example/router.js
@@ -31,23 +31,48 @@ let pagesRoutes = [
   }
 ]
 
-export const componentRoutes = pagesRoutes.find(route => route.name === 'component-page').children = []
+const componentRoutes = pagesRoutes.find(route => route.name === 'component-page').children = []
+
+const groupNameList = []
 
 const requireRouter = require.context('./docs', true, /\.md$/)
 
 requireRouter.keys().forEach(requireName => {
+  // 获取文件路径
   const fileName = requireName.match(/(?<=(\/)).*(?=\.md)/)[0]
-  const eName = fileName.match(/\w+/)[0].toLowerCase()
+  // 分割文件字符串
+  const fileNameList = fileName.split('/')
+  // 得到md名
+  const mdName = fileNameList[fileNameList.length - 1].split(' ')[0].toLowerCase()
+  // 得到文件夹名
+  const eName = fileNameList[0]
+  // 得到分组名
+  const groupName = (fileNameList && fileNameList.length === 3 && fileNameList[1]) || ''
+
   const route = {
     meta: {
-      name: fileName
+      name: fileNameList[fileNameList.length - 1],
+      // 类别名
+      pageName: eName,
+      // 分组名
+      groupName
     },
-    path: eName,
-    name: eName,
+    path: mdName,
+    name: mdName,
     component: (resolve) => require([`./docs/${fileName}.md`], resolve)
   }
   componentRoutes.push(route)
+
+  // 没有添加过则将分组名添加进去
+  !groupNameList.includes(groupName) && groupNameList.push(groupName)
 })
+
+export const groupList = groupNameList.map(v => ({
+  title: v,
+  children: componentRoutes.filter(item => item.meta.groupName && item.meta.groupName === v)
+}))
+
+export const guideList = componentRoutes.filter(v => v.meta.pageName === 'guide')
 
 const router = new VueRouter({
   routes: [ ...pagesRoutes ]
