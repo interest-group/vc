e7f2288a54e4bd32c04d7455286e574e71ff2ee6
zepang <ummlq4@gmail.com>
2020-04-02 20:31:32 +0800

    fix

--- example/App.vue
+++ example/App.vue
@@ -1,11 +1,13 @@
 <template>
   <vc-container class="app">
     <vc-header class="app-header">
-      <router-link to="/"><img class="app-header-logo" alt="logo" src="./assets/logo.png"></router-link>
-      <h1 class="app-header-title heading-middle">Vue Components</h1>
+      <router-link to="/">
+        <img class="app-header-logo" alt="logo" src="./assets/logo.png" />
+        <h1 class="app-header-title heading-middle">Vue Components</h1>
+      </router-link>
       <ul class="app-header-nav">
-        <li><router-link class="heading-small" to="/">Home</router-link></li>
-        <li><router-link class="heading-small" :to="{name: 'layout'}">Components</router-link></li>
+        <li><router-link class="heading-small" :class="{active: !isComponentActive}"  to="/">Home</router-link></li>
+        <li><router-link class="heading-small" :class="{active: isComponentActive}" :to="{name: 'layout'}">Components</router-link></li>
         <li><a class="heading-small" target="_blank" href="https://github.com/interest-group/vc">Github</a></li>
       </ul>
     </vc-header>
@@ -17,71 +19,96 @@
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  computed: {
+    isComponentActive () {
+      return this.$route.matched.some(item => item.path === '/component')
+    }
+  }
 }
 </script>
 
 <style lang="scss">
-  .app {
-    height: 100%;
-    width: 90%;
-    margin: 0 auto;
-    min-width: 1000px;
+::-webkit-scrollbar {
+    background-color: #edf0f5;
+    height: 10px;
+    width: 10px;
+}
+
+::-webkit-scrollbar-thumb {
+    background-color: #ccc;
+    border-radius: 5px;
+}
+
+.app {
+  height: 100%;
+  width: 90%;
+  margin: 0 auto;
+  min-width: 1000px;
+  display: flex;
+  flex-direction: column;
+  &-header {
+    box-sizing: border-box;
+    height: 80px;
+    background-color: #FFF;
     display: flex;
-    flex-direction: column;
-    &-header {
-      box-sizing: border-box;
-      height: 80px;
-      background-color: #FFF;
+    align-items: center;
+    padding: 10px 20px;
+    z-index: 100;
+    border-bottom: 1px solid #e5e5e5;
+    & >  a {
       display: flex;
       align-items: center;
-      padding: 10px 20px;
-      z-index: 100;
-      box-shadow: 0 1px 4px 0 rgba(213,218,224,0.5);
-      &-logo {
-        width: 45px;
-      }
-      &-title {
-        cursor: default;
-        margin: 0 20px;
-      }
-      &-nav {
-        margin-left: auto;
-        li {
-          display: inline-block;
-          padding: 0 20px;
+      height: 45px;
+      cursor: pointer;
+    }
+    &-logo {
+      width: 45px;
+    }
+    &-title {
+      margin: 0 20px;
+    }
+    &-nav {
+      margin-left: auto;
+      li {
+        display: inline-block;
+        padding: 0 20px;
+        position: relative;
+        list-style: none;
+        margin-left: 20px;
+        a {
           position: relative;
-          list-style: none;
-          margin-left: 20px;
-          a {
-            position: relative;
-            color: lighten(#5575c8, 10%);
-            &:after {
-              content: '';
-              display: inline-block;
-              width: 100%;
-              height: 3px;
-              position: absolute;
-              top: 130%;
-              left: 0;
-              background-color: #5575c8;
-              transform-origin: 50%;
-              transform: scaleX(0);
-              transition: all .2s .1s ease-in-out;
-            }
+          color: lighten(#5575c8, 10%);
+          &:after {
+            content: '';
+            display: inline-block;
+            width: 100%;
+            height: 3px;
+            position: absolute;
+            top: 130%;
+            left: 0;
+            background-color: #5575c8;
+            transform-origin: 50%;
+            transform: scaleX(0);
+            transition: all .2s .1s ease-in-out;
           }
-          &:hover a {
+
+          &.active {
             color: #5575c8;
             &:after {
               transform: scaleX(1);
             }
           }
         }
+        &:hover a {
+          color: #5575c8;
+        }
       }
     }
-    &-container {
-      flex: 1;
-      overflow-y: hidden;
-    }
   }
+  &-container {
+    flex: 1;
+    overflow-y: hidden;
+  }
+}
 </style>
